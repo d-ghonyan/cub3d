601984bb3873f0ca66a43c462be11132dca00b2e
Davit Ghonyan <dghonyan@c2r9s3.42yerevan.am>
2022-10-23 20:22:07 +0400

    xpms for minimap

--- src/minimap.c
+++ src/minimap.c
@@ -31,30 +31,36 @@ void	draw_minimap(t_win *win)
 {
 	int		i;
 	int		j;
+	int		w;
+	int		h;
+	
 	int		color;
 	char	*dst;
 
 	i = -1;
-	while (++i < win->map_len * SIDE)
+	void	*img = mlx_xpm_file_to_image(win->mlx, "./minimap/pixel.xpm", &w, &h);
+	if (!img)
+		error("a", 1);
+	while (++i < win->map_len)
 	{
 		j = -1;
-		while (++j < ft_strlen(win->map[i / SIDE]) * SIDE)
+		while (++j < ft_strlen(win->map[i]))
 		{
-			color = 123 << 16 | 0 << 8 | 255;
-			if (win->map[i / SIDE][j / SIDE] == '0')
-				color = 255 << 24 | 23 << 16 | 0 << 8 | 9;
-			else if (win->map[i / SIDE][j / SIDE] == ' ')
-				color = 255 << 24 | 0;
-			else if (i / SIDE == win->player.pos_y_mini
-				&& j / SIDE == win->player.pos_x_mini)
-			{
-				printf("PLAYER\n");
-				color = 255 << 8 | 0;
-			}
-			dst = win->addr_mini + (i * win->line_length_mini
-					+ j * (win->bits_per_pixel_mini / 8));
-			*(unsigned int *)(dst) = color;
+			// color = 123 << 16 | 0 << 8 | 255;
+			if (win->map[i][j] == '1')
+				mlx_put_image_to_window(win->mlx, win->win, img, j * w, i * h);
+			// else if (win->map[i / SIDE][j / SIDE] == ' ')
+			// 	color = 255 << 24 | 0;
+			// else if (i / SIDE == win->player.pos_y_mini
+			// 	&& j / SIDE == win->player.pos_x_mini)
+			// {
+			// 	printf("PLAYER\n");
+			// 	color = 255 << 8 | 0;
+			// }
+			// dst = win->addr_mini + (i * win->line_length_mini
+			// 		+ j * (win->bits_per_pixel_mini / 8));
+			// *(unsigned int *)(dst) = color;
 		}
 	}
-	mlx_put_image_to_window(win->mlx, win->win, win->img_mini, 0, 0);
+	// mlx_put_image_to_window(win->mlx, win->win, win->img_mini, 0, 0);
 }
